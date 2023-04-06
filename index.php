c58c82cd8600ceb80c1536c17fd24ae94a6239fe
lukakone <luka.konecnik@student.um.si>
2023-04-06 09:58:11 +0200

    "index.php connection to db"

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<?php
+$con = mysqli_init();
+mysqli_ssl_set($con, NULL, NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "milijonar.mysql.database.azure.com", "milijonar_koda4", "Koda1234!", "test", 3306, MYSQLI_CLIENT_SSL);
+?>
 
 <body>
     test
