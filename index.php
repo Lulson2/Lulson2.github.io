925f9ddc978b24f4617b3a2ba2017314cca13327
lukakone <luka.konecnik@student.um.si>
2023-04-07 16:43:12 +0200

    "zacetna stran"

--- index.php
+++ index.php
@@ -7,42 +7,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<?php
-// Database connection details
-$host = "sql303.epizy.com";
-$username = "epiz_33963503";
-$password = "0XYpGgygXZVRG";
-$dbname = "epiz_33963503_milijonar";
-
-// Create connection
-$conn = new mysqli($host, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Fetch users from database
-$sql = "SELECT * FROM users";
-$result = $conn->query($sql);
-
-// Display users in a table
-if ($result->num_rows > 0) {
-    echo "<table id='users'><thead><tr><th>Name</th><th>Email</th><th>Role</th></tr></thead><tbody>";
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td><td>" . $row["role"] . "</td></tr>";
-    }
-    echo "</tbody></table>";
-} else {
-    echo "No users found.";
-}
-
-$conn->close();
-?>
 
 <body>
-    test
+    <?php
+    // Database connection details
+    $host = "localhost"; //"sql303.epizy.com";
+    $username = "root"; //"epiz_33963503";
+    $password = ""; //"0XYpGgygXZVRG";
+    $dbname = "milijonar"; //"epiz_33963503_milijonar";
+    
+    // Create connection
+    $conn = new mysqli($host, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Fetch users from database
+    $sql = "SELECT * FROM users";
+    $result = $conn->query($sql);
+
+    // Display users in a table
+    if ($result->num_rows > 0) {
+        echo "<table id='users'><thead><tr><th>Name</th><th>Email</th><th>Role</th></tr></thead><tbody>";
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["username"] . "</td><td>" . $row["trophies"] . "</td><td>" . $row["streak"] . "</td></tr>";
+        }
+        echo "</tbody></table>";
+    } else {
+        echo "No users found.";
+    }
+
+    $conn->close();
+    ?>
 </body>
 
 </html>
